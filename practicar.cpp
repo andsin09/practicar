f91d1c0ac6364d840f4e48cf02a2b9b35d12466d
andsin09 <161988024+andsin09@users.noreply.github.com>
2024-10-15 09:21:15 -0400

    asfadsfasd

---
+++ practicar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+string name = " ";
+int age;
+char initial;
+float grade;
+printf("Enter in order of name, age, last initial, and GPA.\n");
+printf(" ");
+scanf("%s\n", &name );
+scanf("%d\n", &age );
+scanf("%c\n", &initial );
+scanf("%f\n", &grade );
+
+}
