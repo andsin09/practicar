9596cd8f1be5148315a99453e1cac41171dd5aa2
andsin09 <161988024+andsin09@users.noreply.github.com>
2024-10-17 08:17:54 -0400

    cleaned up

--- practicar.cpp
+++ practicar.cpp
@@ -2,17 +2,23 @@
 #include <string>
 using namespace std;
 int main(){
-char name[100];
-int age;
-char initial;
-float grade;
-printf("First Name, Age, Initial, Grade Average.");
-scanf("%s\n", &name );
-scanf("%d\n", &age );
-scanf("%c\n", &initial );
-scanf("%f", &grade );
-printf("Hello, %s\n", name);
-printf("Last initial: %c\n", initial);
-printf("Age: %d\n", age);
-printf("Grade Average: %.2f\n", grade);
+  
+  char name[100];
+  int age;
+  char initial;
+  float grade;
+  
+  printf("First Name, Age, Initial, Grade Average.");
+  
+  scanf("%s\n", &name );  
+  scanf("%d\n", &age );
+  scanf("%c\n", &initial );
+  scanf("%f", &grade );
+  
+  printf("Hello, %s\n", name);
+  printf("Last initial: %c\n", initial);
+  printf("Age: %d\n", age);
+  printf("Grade Average: %.2f\n", grade);
+
+  return 0;
 }
