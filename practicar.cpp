e96039fb40140c30ab499bcc1dde980be55723d9
andsin09 <161988024+andsin09@users.noreply.github.com>
2024-10-17 08:16:42 -0400

    Update practicar.cpp

--- practicar.cpp
+++ practicar.cpp
@@ -6,7 +6,7 @@ char name[100];
 int age;
 char initial;
 float grade;
-printf("First Name, Age, Initial, GPA.");
+printf("First Name, Age, Initial, Grade Average.");
 scanf("%s\n", &name );
 scanf("%d\n", &age );
 scanf("%c\n", &initial );
@@ -14,5 +14,5 @@ scanf("%f", &grade );
 printf("Hello, %s\n", name);
 printf("Last initial: %c\n", initial);
 printf("Age: %d\n", age);
-printf("GPA: %.2f\n", grade);
+printf("Grade Average: %.2f\n", grade);
 }
